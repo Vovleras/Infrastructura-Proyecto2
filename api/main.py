185e371786e3d063207c4c994cfb42c3217ceeb0
Vovleras <victoria.volveras@correounivalle.edu.co>
2025-07-30 18:10:44 -0500

    update HTTP url

--- api/main.py
+++ api/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=["http://34.228.38.150:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
